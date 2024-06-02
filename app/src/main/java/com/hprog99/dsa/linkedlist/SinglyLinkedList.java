8748a5a6f436123042803703c9651058586f2987
Hiten Pratap Singh <hitenpratap99@gmail.com>
2024-06-02 23:47:47 +0200

    feat: Implement SinglyLinkedList in Java

---
+++ app/src/main/java/com/hprog99/dsa/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,132 @@
+package com.hprog99.dsa.linkedlist;
+
+/**
+ * SinglyLinkedList
+ */
+public class SinglyLinkedList {
+
+    private static class Node {
+
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public SinglyLinkedList() {
+        this.size = 0;
+        this.head = null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public boolean contains(int data) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public void addAtHead(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addAtTail(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            addAtHead(data);
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int data, int index) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            addAtHead(data);
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        Node current = head;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node prev = null;
+            for (int i = 0; i < index; i++) {
+                prev = current;
+                current = current.next;
+            }
+            prev.next = current.next;
+        }
+        size--;
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addAtHead(1);
+        list.addAtTail(2);
+        list.addAtTail(3);
+        list.addAtTail(4);
+        list.print();
+        list.addAtIndex(5, 3);
+        list.print();
+        list.remove(3);
+        list.print();
+    }
+}
